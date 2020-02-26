28f1ae211165686d0e025652792b0079d463770f
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-02-26 22:57:09 +0900

    implement client

---
+++ src/client/client.ts
@@ -0,0 +1,19 @@
+class Client {
+  constructor(
+    private _idealApplicationService: IdealApplicationServiceInterface
+  ) {}
+
+  getAll(): string[][] {
+    return this._idealApplicationService.getAll();
+  }
+
+  create(item: string): void {
+    const command = new IdealCreateCommand(item);
+    this._idealApplicationService.create(command);
+  }
+
+  delete(id: string): void {
+    const command = new IdealDeleteCommand(id);
+    this._idealApplicationService.delete(command);
+  }
+}
