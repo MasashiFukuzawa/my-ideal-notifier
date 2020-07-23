6652d2e6143b928404bbdc2b682546e307ad4845
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-07-23 16:43:49 +0900

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,7 @@ import { IdealRepository } from './repository/ideal_repository';
 import { IdealApplicationService } from './application/service/ideal_application_service';
 import { Client } from './client/client';
 
-declare namespace glFunctions {
-  interface global {
-    main(): void;
-  }
-}
-
-declare var global: glFunctions.global;
-
-// 最終的にGAS側で `function main() {...}`という形に修正する必要あり
-global.main = (): void => {
+function main(): void {
   const idealRepository = new IdealRepository();
   const idealApplicationService = new IdealApplicationService(idealRepository);
   const client = new Client(idealApplicationService);
